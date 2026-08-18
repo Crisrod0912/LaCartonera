32b99c578bda42b120ca0dee05249597b77e0094
Crisrod0912 <rodriguezfernandezcristopher@gmail.com>
2026-08-17 21:15:25 -0600

    Updates y config adicional

---
+++ LaCartonera.Web/LaCartonera.Web/Data/IMongoDBContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace LaCartonera.Web.Data
+{
+    public interface IMongoDBContext
+    {
+        IMongoClient CreateConnection();
+    }
+}
---
+++ LaCartonera.Web/LaCartonera.Web/Data/MongoDBContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace LaCartonera.Web.Data
+{
+    public class MongoDBContext : IMongoDBContext
+    {
+        private readonly string? _variable;
+
+        public MongoDBContext(IConfiguration configuration)
+        {
+            _variable = configuration["urlWebApi"]
+                ?? configuration["Default"]
+                ?? throw new InvalidOperationException("Connection string 'urlWebApi' not found in configuration (appsettings.json).");
+        }
+
+        public IMongoClient CreateConnection()
+            => new MongoClient(_variable);
+    }
+}
